3bdc2e6552af89b0815aae3ba1df4c124b1fa43d
Kyle Lee <kyle.c.lee.813@gmail.com>
2023-05-31 22:22:52 -0600

    Add arrays and function

---
+++ random_quote/random-quote.js
@@ -0,0 +1,7 @@
+const characteristic = ['smile','personality','joy', 'spirit','aura','patience','countenance','passion','loyalty']
+const state = ['success','happiness','divinity','enlightenment','nirvana','bliss','serenity','peace']
+
+function generateFortune(arr1, arr2) {
+    
+    console.log(`Your ${arr1} pushes others on to ${arr2}`)
+}
